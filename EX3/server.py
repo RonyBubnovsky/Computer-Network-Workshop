8af831dbd45715f92e9882f78240fe16263904bb
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2024-06-28 12:31:46 +0300

    added clinet.py

--- EX3/server.py
+++ EX3/server.py
@@ -64,37 +64,53 @@ def try_connecting_to_other_servers():
                 connect_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
                 connect_sock.connect(('127.0.0.1', port))
                 print(f"{chosen_port} connected to {port} successfully. requesting from {port} its connected servers list...\n")
-                servers_im_connected_to[port] = connect_sock.getsockname() 
+                servers_im_connected_to[port] = connect_sock 
                 clique_ports = ask_for_clique(connect_sock, chosen_port) # Ask for the clique of the other server
                 connect_to_servers_in_the_clique(clique_ports, chosen_port) # Connect to the other servers in the received clique
                 found_listening_server = True
             except ConnectionRefusedError:
                 print(f"No server is listening on port {port}")
-            
+  
+  
+def handle_clique_request(conn_socket, client_address):
+    print("request to send clique unpacked\n")
+    port_to_add = conn_socket.recv(4) # the port i need to add to my dict
+    servers_im_connected_to[int((port_to_add.decode()))] = conn_socket # add the port that connects to me to my dict
+    print("The dict i need to send is: ", servers_im_connected_to, '\n')
+    ip_and_ports_string = ""
+    if len(servers_im_connected_to) > 0:
+        for port, address in servers_im_connected_to.items():
+            ip_and_ports_string += str(address[0]) + ':' + str(port) + '\0'
+        data_to_unpack = struct.pack('>bbhh', 1, 0, len(ip_and_ports_string)-1, 0)
+        print("clique to send packed\n")
+        conn_socket.send(data_to_unpack)
+        conn_socket.send(ip_and_ports_string[:-1].encode())
+        print("clique to send sent\n")         
 
+
+def handle_new_connection_from_client(conn_socket, length):
+     connected_clients[conn_socket.recv(length).decode()] = conn_socket # adding the client and the connection to the client dictionary
+     print("My connected clients are: ", connected_clients, '\n')
+     conn_socket.send(struct.pack('>bbhh', 2, 0, 0, 0))
+     print("Sent to the client that i added his name to my dictionary\n")
+     
+    
+    
 threading.Thread(target=try_connecting_to_other_servers).start()
 def respond_to_client(conn_socket, client_address):
     print('start listening from', client_address)
     data = conn_socket.recv(6)
     type, subtype, length, sublen = struct.unpack('>bbhh', data)
     if type == 0 and subtype == 0: # the other server requested my clique
-        print("request to send clique unpacked\n")
-        port_to_add = conn_socket.recv(4) # the port i need to add to my dict
-        servers_im_connected_to[int((port_to_add.decode()))] = client_address # add the port that connects to me to my dict
-        print("The dict i need to send is: ", servers_im_connected_to, '\n')
-        ip_and_ports_string = ""
-        if len(servers_im_connected_to) > 0:
-            for port, address in servers_im_connected_to.items():
-                ip_and_ports_string += str(address[0]) + ':' + str(port) + '\0'
-            data_to_unpack = struct.pack('>bbhh', 1, 0, len(ip_and_ports_string)-1, 0)
-            print("clique to send packed\n")
-            conn_socket.send(data_to_unpack)
-            conn_socket.send(ip_and_ports_string[:-1].encode())
-            print("clique to send sent\n")
+        handle_clique_request(conn_socket, client_address)
+    if type == 2 and subtype == 1: # recieved new connection header from client
+        handle_new_connection_from_client(conn_socket, length)
+        
+        
 
 while True:
     conn, client_address = sock.accept()
-    print('new connection from', client_address, conn)
+    print('new connection from', client_address)
     threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
     
    
